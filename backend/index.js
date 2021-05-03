fc115eed45758eb5c484695e3bb6920135da26da
Un-dev <francois.lannoy@kelindi.com>
2021-05-04 00:33:12 +0200

    :sparkles: added basic routes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
+const data = require("./data/data.json")
+
 const fastify = require("fastify")({
   logger: true,
 })
 
 fastify.get("/", function (request, reply) {
   reply.send({ hello: "world" })
+})
+
+fastify.get("/books", function (request, reply) {
+  reply.send({ books: data.books })
+})
+
+fastify.get("/users/:id", function (request, reply) {
+  reply.send({ user: data.users[request.params.id] })
+})
+
+fastify.get("/users/:id/books", function (request, reply) {
+  borrowed_books = data.books.filter((b) => b.borrowed_by == request.params.id)
+  reply.send({ userBorrowed: borrowed_books })
 })
 
 fastify.listen(3000, function (err, address) {
